7ffe3bc0389d301b0863915e326189493c5a0023
TyPetrochko <ty.petrochko@gmail.com>
2014-11-04 11:02:00 -0500

    Update posts_controller.rb

--- Froyolo/app/controllers/posts_controller.rb
+++ Froyolo/app/controllers/posts_controller.rb
@@ -1,15 +1,40 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-  
-  
+  require 'open-uri'
+
   respond_to :json
   # GET /posts
   # GET /posts.json
   def index
     @posts = Post.all
   end
-  
+
   def find_importance
+
+
+    @posts = Post.where("xcoord > ?", search_params[:minx]).where("xcoord < ?", search_params[:maxx])
+    @posts = @posts.where("ycoord > ?", search_params[:miny]).where("ycoord < ?", search_params[:maxy])
+
+    lowest_possible_value = -3
+    sum_importance = 0
+    totalage = 0
+    votespersecond = 1 / 36000
+
+    @posts.each do |p|
+      p.age = (Time.zone.now - p.created_at) * votespersecond
+      totalage += p.age
+    end
+
+    @posts.each do |p|
+      if p.age / totalage > 0.5
+        p.ageadjustedscore = p.score - p.age * 2.0
+      else
+        p.ageadjustedscore = p.score - p.age / 2.0
+      end
+    end
+
+    @posts.each do |p|
+      sum_importance += (p.ageadjustedscore - lowest_possible_value)
     lowest_possible_value = -3
     @posts = Post.all
     sum_importance = 0
@@ -18,9 +43,18 @@ class PostsController < ApplicationController
     @posts.each do |p|
       p.age = (Time.zone.now - p.created_at) / 3600 
       totalage += p.age
+
     end
-    
+
     @posts.each do |p|
+
+      if p.ageadjustedscore < lowest_possible_value + 1
+        p.importance = 0
+      else
+        p.importance = 100 / 93.41 * (1.0 - Math.exp( -1 * (-1.0 * lowest_possible_value + p.ageadjustedscore) / sum_importance * Math.exp(1)))
+      end
+    end
+
       if p.age / totalage > 0.5
         p.ageadjustedscore = p.score - p.age * 2.0
       else
@@ -41,7 +75,6 @@ class PostsController < ApplicationController
     end  
     @posts
   end
-  
 
   #POST /increment/1.json
   def increment
@@ -100,10 +133,10 @@ class PostsController < ApplicationController
   def show
   end
 
-  #def show_all
-  #  @posts = Post.all
-  #  @post = Post.first
-  #end
+  def show_all
+    @posts = Post.all
+    @post = Post.first
+  end
 
   # GET /posts/new
   def new
@@ -119,6 +152,18 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     @post.score = 0
+
+
+
+    if(post_params[:image] == nil)
+      path = "#{Rails.root}/public/uploads/post/image/#{@post.id}/image.jpg"
+      File.open(path, "wb") do |f|
+        f.write(Base64.decode64(@post.imagedata))
+      end
+      @post.image = File.open(path)
+    else
+      @post.imagedata = Base64.encode64(File.open(@post.image.path).read)
+    end
 
     respond_to do |format|
       if @post.save
@@ -167,7 +212,7 @@ class PostsController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def post_params
-    params.require(:post).permit(:xcoord, :ycoord, :altitude, :horizontalaccuracy, :verticalaccuracy, :image)
+    params.require(:post).permit(:xcoord, :ycoord, :altitude, :horizontalaccuracy, :verticalaccuracy, :image, :imagedata)
   end
 
   def search_params
